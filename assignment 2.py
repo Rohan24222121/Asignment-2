d715042df9a65302b6914461ea57b860ea671f8a
Rohan24222121 <136613540+Rohan24222121@users.noreply.github.com>
2023-06-20 09:40:04 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,15 @@
+a = 5
+b = 4
+
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a /b)   
+
